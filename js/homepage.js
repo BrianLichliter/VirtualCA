53d433db8192a4046e1920e8df57897b3f3e59da
BrianLichliter <brian.lichliter@gmail.com>
2014-02-26 19:25:02 -0600

    formatting fix

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 	$('#formselection').click(function() {
-    	var form = $( "#formselect" ).val();
-    	if (form == "duty") {
-    		window.location.href = "DutyLog.html";
-    	}
+		var form = $( "#formselect" ).val();
+		if (form == "duty") {
+			window.location.href = "dutylog.brian.html";
+		}
+	});
+
+	$(window).scroll(
+	{
+	}, 
+	function () {
+		var currentTop = $(window).scrollTop();
+		if (currentTop < 50) {
+			$("#fixed").fadeIn();
+		}
+		else if (currentTop > 50) {
+			$("#fixed").fadeOut();
+		}
 	});
 });
